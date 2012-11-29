584a47912cba816f68c75a22571bc217558c451e
Johnny Dean <johnny.d.dean@gmail.com>
2012-11-28 20:09:26 -0800

    Add short name and long name for the cards and fixup ranks

--- cards.py
+++ cards.py
@@ -7,6 +7,17 @@ class Card:
         self.rankID = rankID
         self.rank = ranks[rankID-1]
         self.suit = suits[suitID-1]
+        self.longName = self.rank + " of " + self.suit
+        self.shortName = self.rank[0] + self.suit[0]
+        if self.rank == "10":
+            self.shortName = "10"+self.suit[0]
+
+        if self.rank == "Ace":
+            self.value = 1
+        elif self.rank in ["Jack", "Queen", "King"]:
+            self.value = 10
+        else:
+            self.value = int(self.rank)
 
 deck = []
 for rank in range(1,14):
@@ -14,3 +25,7 @@ for rank in range(1,14):
         deck.append(Card(rank,suit))
 
 print "The deck has", len(deck), "cards in it."
+
+
+for card in deck:
+    print card.shortName,card.longName,"Value:",card.value
